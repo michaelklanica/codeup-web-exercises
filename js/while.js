b38f7643b548d43a12dae12fc8fd26ab546f774b
Michael Klanica <michael.david.klanica@gmail.com>
2020-07-30 13:36:22 -0500

    added while exercises

---
+++ js/while.js
@@ -0,0 +1,12 @@
+"use strict";
+(function() {
+
+    var numOfIterations = 1, num = 1;
+    while (numOfIterations <= 16) {
+        num *= 2;
+        console.log(num);
+        numOfIterations++;
+    }
+
+
+})();
