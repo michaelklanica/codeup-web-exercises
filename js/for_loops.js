a5e751933ec76215cdb67b5125eb3c89e32b41ad
Michael Klanica <michael.david.klanica@gmail.com>
2020-07-30 12:15:47 -0500

    loop exercises

---
+++ js/for_loops.js
@@ -0,0 +1,35 @@
+"use strict";
+(function() {
+    //For Loop Exercise 2
+    function showMultiplicationTable (num) {
+        for (var x = 1; x <= 10; x++) {
+            console.log(num + " x " + x + " = " + (num * x));
+        }
+    }
+    showMultiplicationTable(5);
+
+    //For Loop Exercise 3
+    function isEvenOrOdd() {
+        for (var x = 1; x <= 10; x++) {
+            var random = Math.floor((Math.random() * 200) + 20);
+            if (random % 2 === 0) {
+                console.log(random + " is even.");
+            } else {
+                console.log(random + " is odd.");
+            }
+        }
+    }
+    isEvenOrOdd();
+
+    //For Loop Exercise 4
+    for (var x = 1; x < 10; x++) {
+        console.log(x.toString().repeat([x]));
+    }
+
+
+
+
+
+
+
+})();
