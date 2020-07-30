9d4ea42d4c5793f1d5c385c14650e4b71afcdeb1
Michael Klanica <michael.david.klanica@gmail.com>
2020-07-30 14:31:53 -0500

    commit changes to for and while loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -32,5 +32,7 @@
     for (var x = 100; x > 0; x -= 5) {
         console.log(x);
     }
-    
+
+
+
 })();
--- js/while.js
+++ js/while.js
@@ -8,5 +8,18 @@
         numOfIterations++;
     }
 
+    var allCones = Math.floor(Math.random() * 50) + 50;
+    var conesSold = 0;
+    console.log("You have " + allCones + " left to sell.");
+    do {
+        conesSold = Math.floor(Math.random() * 5) + 1;
+        if (allCones - conesSold >= 0) {
+            allCones -= conesSold;
+            console.log(conesSold + " cones sold...");
+            continue;
+        }
+        console.log("I cannot sell you " + conesSold + ".  I only have " + allCones + " left to sell.");
+    } while(allCones > 0);
+    console.log("Yay! I sold them all!");
 
 })();
