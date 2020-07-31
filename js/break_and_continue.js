5f506992c876ac7f5e721a8634208f0e58fc5fa8
Michael Klanica <michael.david.klanica@gmail.com>
2020-07-31 09:55:44 -0500

    updates to break_and_continue.js and for_loops.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,16 +1,19 @@
 "use strict";
 (function() {
 
-    var input = prompt("Enter an ODD number between 1 and 50.");
+    var input;
 
-    while (input > 50 || input % 2 === 0) {
-        input = prompt("Enter an ODD number between 1 and 50.");
+    while (true) {
+        input = parseInt(prompt("Enter an ODD number between 1 and 50."));
+        if (input % 2 !== 0 && input > 0 && input < 51) {
+            break;
+        }
     }
 
     console.log("Number to skip is " + input);
 
     for (var x = 1; x < 50; x += 2) {
-        if (x === parseInt(input)) {
+        if (x === input) {
             console.log("Yikes! Skipping number: " + input);
             continue;
         }
--- js/for_loops.js
+++ js/for_loops.js
@@ -8,12 +8,12 @@
             console.log(num + " x " + x + " = " + (num * x));
         }
     }
-    showMultiplicationTable(5);
+    showMultiplicationTable(7);
 
     //For Loop Exercise 3
     function isEvenOrOdd() {
         for (var x = 1; x <= 10; x++) {
-            var random = Math.floor((Math.random() * 200) + 20);
+            var random = Math.floor((Math.random() * 181) + 20);
             if (random % 2 === 0) {
                 console.log(random + " is even.");
             } else {
@@ -25,7 +25,15 @@
 
     //For Loop Exercise 4
     for (var x = 1; x < 10; x++) {
-        console.log(x.toString().repeat([x]));
+        console.log(x.toString().repeat(x));
+    }
+
+    for (var i = 1; i < 10; i++){ 
+        var display = ""; 
+        for(var j = 0; j < i; j++){ 
+            display += i; 
+        } 
+        console.log(display); 
     }
 
     //For Loop Exercise 5
