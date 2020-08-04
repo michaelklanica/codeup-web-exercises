49b686f17b3e419eae3452d651149b7d614e961e
Michael Klanica <michael.david.klanica@gmail.com>
2020-08-04 12:45:11 -0500

    added assessment#2 solutions for study

--- js/sampleJavascript.js
+++ js/sampleJavascript.js
@@ -21,6 +21,100 @@
        console.log(model);
     });
 
+    // Assessment #2
+    // Solution #1
+    function isANumber(value) {
+        if (typeof value === "boolean" || value === null) {
+            return false;
+        }
+        return true;
+    }
+
+    // Solution #2
+    function increment(value) {
+        if(isANumber(value)) {
+            return value + 1;
+        } else {
+            return false;
+        }
+    }
+
+    // Solution #3
+    function decrement(value) {
+        if(isANumber(value)) {
+            return value - 1;
+        } else {
+            return false;
+        }
+    }
+
+    // Solution #4
+    function getHighestNumber(value1, value2, value3) {
+        if (isNaN(value1) || isNaN(value2) || isNaN(value3)) {
+            return false;
+        }
+        // var highestNumber = value1;
+        // if (value2 > highestNumber) {
+        //     highestNumber =value2;
+        // }
+        // if (value3 > highestNumber) {
+        //     highestNumber =value3;
+        // }
+        // return highestNumber;
+        return Math.max(value1, value2, value3); //use above or this line.
+    }
+
+    // Solution #5
+    function parseNumber(value) {
+        return parseFloat(value);
+    }
+
+    // Solution #6
+    function add(value1, value2) {
+        if (isANumber(value1) && isANumber(value2)) {
+            return parseFloat(value1) + parseFloat(value2);
+        } else {
+            return false;
+        }
+    }
+
+    // Solution #7
+    function multiply(value1, value2) {
+        if (isANumber(value1) && isANumber(value2)) {
+            return parseFloat(value1) * parseFloat(value2);
+        } else {
+            return false;
+        }
+    }
+
+    // Solution #8
+    function square(value) {
+        if (isANumber(value)) {
+            return parseNumber(value) * parseNumber(value);
+        }
+        return false;
+    }
+
+    // Solution #9
+    function sumOfSquares(value1, value2) {
+        if (isANumber(value1) && isANumber(value2)) {
+            return square(value1) + square(value2);
+        }
+        return false;
+    }
+    // Solution #10
+    function isPalindrome(input) {
+        if (typeof input !== "string") {
+            return false;
+        }
+        if (input.toLowerCase() === input.split('').reverse().join('')) {
+            return true;
+        }
+        return false;
+    }
+
+
+
 
 
 
