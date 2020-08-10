966c40150484a6b1eaf4e4e632b8eb131cf0c4d0
Michael Klanica <michael.david.klanica@gmail.com>
2020-08-10 15:45:59 -0500

    css selectors exercise 1

--- js/sampleJavascript.js
+++ js/sampleJavascript.js
@@ -1,117 +1,117 @@
 "use strict";
 (function() {
-
-    var car1 = ["Ford", "Escort", "silver", 1991, "manual"];
-    var car2 = ["Ford", "Escort", "green", 1998, "automatic"];
-    var car3 = ["Kia", "Rio", "red", 2003, "automatic"];
-    var car4 = ["Toyota", "4Runner", "white", 2003, "automatic"];
-    var car5 = ["Toyota", "FJ Cruiser", "silver", 2008, "automatic"];
-
-    var someArray = [car1, car2, car3, car4, car5];
-
-
-    for (var i = 0; i < someArray.length; i++) {
-        // console.log(someArray[i], typeof someArray[i]);
-        for (var x = 0; x < someArray[i].length; x++) {
-            console.log(someArray[i][x], typeof someArray[i][x]);
-        }
-    }
-
-    car1.forEach(function(model) {
-       console.log(model);
-    });
-
-    // Assessment #2
-    // Solution #1
-    function isANumber(value) {
-        if (typeof value === "boolean" || value === null) {
-            return false;
-        }
-        return true;
-    }
-
-    // Solution #2
-    function increment(value) {
-        if(isANumber(value)) {
-            return value + 1;
-        } else {
-            return false;
-        }
-    }
-
-    // Solution #3
-    function decrement(value) {
-        if(isANumber(value)) {
-            return value - 1;
-        } else {
-            return false;
-        }
-    }
-
-    // Solution #4
-    function getHighestNumber(value1, value2, value3) {
-        if (isNaN(value1) || isNaN(value2) || isNaN(value3)) {
-            return false;
-        }
-        // var highestNumber = value1;
-        // if (value2 > highestNumber) {
-        //     highestNumber =value2;
-        // }
-        // if (value3 > highestNumber) {
-        //     highestNumber =value3;
-        // }
-        // return highestNumber;
-        return Math.max(value1, value2, value3); //use above or this line.
-    }
-
-    // Solution #5
-    function parseNumber(value) {
-        return parseFloat(value);
-    }
-
-    // Solution #6
-    function add(value1, value2) {
-        if (isANumber(value1) && isANumber(value2)) {
-            return parseFloat(value1) + parseFloat(value2);
-        } else {
-            return false;
-        }
-    }
-
-    // Solution #7
-    function multiply(value1, value2) {
-        if (isANumber(value1) && isANumber(value2)) {
-            return parseFloat(value1) * parseFloat(value2);
-        } else {
-            return false;
-        }
-    }
-
-    // Solution #8
-    function square(value) {
-        if (isANumber(value)) {
-            return parseNumber(value) * parseNumber(value);
-        }
-        return false;
-    }
-
-    // Solution #9
-    function sumOfSquares(value1, value2) {
-        if (isANumber(value1) && isANumber(value2)) {
-            return square(value1) + square(value2);
-        }
-        return false;
-    }
-    // Solution #10
-    function isPalindrome(input) {
-        if (typeof input !== "string") {
-            return false;
-        }
-        if (input.toLowerCase() === input.split('').reverse().join('')) {
-            return true;
-        }
-        return false;
-    }
+    //
+    // var car1 = ["Ford", "Escort", "silver", 1991, "manual"];
+    // var car2 = ["Ford", "Escort", "green", 1998, "automatic"];
+    // var car3 = ["Kia", "Rio", "red", 2003, "automatic"];
+    // var car4 = ["Toyota", "4Runner", "white", 2003, "automatic"];
+    // var car5 = ["Toyota", "FJ Cruiser", "silver", 2008, "automatic"];
+    //
+    // var someArray = [car1, car2, car3, car4, car5];
+    //
+    //
+    // for (var i = 0; i < someArray.length; i++) {
+    //     // console.log(someArray[i], typeof someArray[i]);
+    //     for (var x = 0; x < someArray[i].length; x++) {
+    //         console.log(someArray[i][x], typeof someArray[i][x]);
+    //     }
+    // }
+    //
+    // car1.forEach(function(model) {
+    //    console.log(model);
+    // });
+    //
+    // // Assessment #2
+    // // Solution #1
+    // function isANumber(value) {
+    //     if (typeof value === "boolean" || value === null) {
+    //         return false;
+    //     }
+    //     return true;
+    // }
+    //
+    // // Solution #2
+    // function increment(value) {
+    //     if(isANumber(value)) {
+    //         return value + 1;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+    //
+    // // Solution #3
+    // function decrement(value) {
+    //     if(isANumber(value)) {
+    //         return value - 1;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+    //
+    // // Solution #4
+    // function getHighestNumber(value1, value2, value3) {
+    //     if (isNaN(value1) || isNaN(value2) || isNaN(value3)) {
+    //         return false;
+    //     }
+    //     // var highestNumber = value1;
+    //     // if (value2 > highestNumber) {
+    //     //     highestNumber =value2;
+    //     // }
+    //     // if (value3 > highestNumber) {
+    //     //     highestNumber =value3;
+    //     // }
+    //     // return highestNumber;
+    //     return Math.max(value1, value2, value3); //use above or this line.
+    // }
+    //
+    // // Solution #5
+    // function parseNumber(value) {
+    //     return parseFloat(value);
+    // }
+    //
+    // // Solution #6
+    // function add(value1, value2) {
+    //     if (isANumber(value1) && isANumber(value2)) {
+    //         return parseFloat(value1) + parseFloat(value2);
+    //     } else {
+    //         return false;
+    //     }
+    // }
+    //
+    // // Solution #7
+    // function multiply(value1, value2) {
+    //     if (isANumber(value1) && isANumber(value2)) {
+    //         return parseFloat(value1) * parseFloat(value2);
+    //     } else {
+    //         return false;
+    //     }
+    // }
+    //
+    // // Solution #8
+    // function square(value) {
+    //     if (isANumber(value)) {
+    //         return parseNumber(value) * parseNumber(value);
+    //     }
+    //     return false;
+    // }
+    //
+    // // Solution #9
+    // function sumOfSquares(value1, value2) {
+    //     if (isANumber(value1) && isANumber(value2)) {
+    //         return square(value1) + square(value2);
+    //     }
+    //     return false;
+    // }
+    // // Solution #10
+    // function isPalindrome(input) {
+    //     if (typeof input !== "string") {
+    //         return false;
+    //     }
+    //     if (input.toLowerCase() === input.split('').reverse().join('')) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
 
 
 
