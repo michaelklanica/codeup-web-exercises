54018e43c0844dcc33a9d2698b4efaf14a6c97bf
Michael Klanica <michael.david.klanica@gmail.com>
2020-12-16 20:51:26 -0600

    building a function for capstone to test here

--- js/sampleJavascript.js
+++ js/sampleJavascript.js
@@ -113,7 +113,55 @@
     //     return false;
     // }
 
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/extract?url=http%3A%2F%2Fwww.melskitchencafe.com%2Fthe-best-fudgy-brownies%2F",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "faf8653d55msh655fdc5ed2b3d42p1c7eb7jsnc0bcfb418e93",
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+        }
+    };
 
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+
+    let allRecipes = getAllRecipes()
+    const recipe = {
+        id: '',
+        name: '',
+        description: '',
+        ingredients: []
+    }
+
+//Add ingredients to the recipe's ingredient array
+    document.querySelector('#ingredient-form').addEventListener('submit', (e) => {
+        e.preventDefault()
+        addIngredient(e, recipe.ingredients);
+        e.target.elements[0].value = ''
+        renderIngredients(recipe.ingredients)
+    })
+
+//Retrieve recipe items, save to local storage, and redirect to home page
+    document.querySelector('#add-recipe').addEventListener('click', () => {
+        const recipeName = document.querySelector('#recipe-name')
+        const recipeDescription = document.querySelector('#recipe-description')
+        recipe.id = window.location.hash.substr(1)
+        if (recipeName.value.length === 0){
+            recipe.name = 'Unnamed Recipe'
+        } else {
+            recipe.name = recipeName.value
+        }
+        recipe.description = recipeDescription.value
+        //push recipe to allRecipes
+        allRecipes.push(recipe)
+        //store allRecipes into local storage
+        saveRecipes(allRecipes)
+        //Redirect to home page
+        window.location.assign('./index.html')
+    })
 
 
 
